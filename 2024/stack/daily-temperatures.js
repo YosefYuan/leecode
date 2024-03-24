419a559bac470a939110fc75927a3212c7726d42
Yosef <yuanruixiangjet@163.com>
2024-03-23 18:10:32 -0700

    feat: stack

---
+++ 2024/stack/daily-temperatures.js
@@ -0,0 +1,5 @@
+// Find the distance to next greater element for each arr[i]
+// Stack Definition: All the elements(index) to the right of arr[i] that are greater than arr[i] (monotone increasing stack)
+//  Top of Stack: Next greater elemnet to the right of arr[i]
+
+ 
